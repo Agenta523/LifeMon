8b046bef41387e3ae3520f669e101fb251d4e7a0
YutaMaekawa <mi201331@edu.okinawa-ct.ac.jp>
2025-05-21 11:38:39 +0900

    パスエラー解消と分析画面の修正

--- lib/presentation/screen/food_screen/other_reg.dart
+++ lib/presentation/screen/food_screen/other_reg.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:life_mon/widget/food_register_template.dart';
+import 'package:life_mon/presentation/widget/food_register_template.dart';
 
 class OtherScreen extends StatelessWidget {
   const OtherScreen({Key? key}) : super(key: key);
--- lib/presentation/screen/food_screen/vegetable_reg.dart
+++ lib/presentation/screen/food_screen/vegetable_reg.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:life_mon/widget/food_register_template.dart';
+import 'package:life_mon/presentation/widget/food_register_template.dart';
 
 class VegetableScreen extends StatelessWidget {
   const VegetableScreen({Key? key}) : super(key: key);
