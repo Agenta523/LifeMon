40f399bffd4b8121b039f50d022f796f115391a7
ikei-NITOC <ac254601@edu.okinawa-ct.ac.jp>
2025-06-19 10:34:26 +0900

    dev

--- lib/presentation/screen/home_screen/home_screen.dart
+++ lib/presentation/screen/home_screen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:life_mon/presentation/widget/calorie_bar.dart';
 import 'package:life_mon/presentation/widget/body_icon.dart';
-import 'package:life_mon/presentation/widget/eat_value.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,48 +12,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<IconData> icons = [Icons.fitness_center, Icons.bolt, Icons.school];
   List<String> labels = ["タンパク質", "脂質", "炭水化物"];
-  List<int> eat_value = [20, 30, 50];
+  List<int> eat_value = [60, 30, 120];
+  List<int> eat_value_base = [90, 60, 250];
+
   final colors_list = <Color>[
     Color.fromARGB(255, 255, 123, 134),
     Color.fromARGB(255, 255, 187, 0),
     Color.fromARGB(255, 123, 134, 255),
   ];
 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    int selectedIndex = 0;
-
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
-    final ellipseWidth = screenWidth * 2.5;
-    final ellipseHeight = screenHeight * 0.7;
-    final ellipseLeft = (screenWidth - ellipseWidth) / 2;
-    final ellipseTop = screenHeight * 0.4;
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Positioned(
-              top: screenHeight * 0.1,
-              left: screenWidth * 0.1,
-              right: 0,
-              child: BodyIcons(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'lib/presentation/images/background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              CalorieBar(),
+              BodyIcons(
                 icons: icons,
                 labels: labels,
                 colors_list: colors_list,
                 selectedIndex: selectedIndex,
+                icons_length: icons.length,
+                eat_values: eat_value,
+                eat_values_base: eat_value_base,
                 onSelected: (index) {
                   setState(() {
                     selectedIndex = index;
                   });
                 },
               ),
-            ),
-            CalorieBar(), // カロリーバーを表示
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
