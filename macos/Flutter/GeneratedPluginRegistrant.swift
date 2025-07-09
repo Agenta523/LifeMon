e7306b2402eb57d03285e0596c18d6a89b86564c
Agenta <shin.age0523@gmail.com>
2025-07-09 10:22:45 +0900

    いらないディレクトリの削除

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import rive_common
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> c5e15e3 (ディレクトリ構造管理)
 }
