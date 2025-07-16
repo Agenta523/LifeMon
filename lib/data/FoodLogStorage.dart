bf9e5b77cc5936d1c6333f638c7f831064a68d94
Agenta <shin.age0523@gmail.com>
2025-07-16 11:28:26 +0900

    FoodLogStorageの修正 - PFC対応, ロード可

--- lib/data/FoodLogStorage.dart
+++ lib/data/FoodLogStorage.dart
@@ -1,35 +1,51 @@
 import 'dart:convert';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DateValueStorage {
-  static const _key = 'date_value';
-  final Map<String, int> _data = {};
+  static const _key = 'daily_nutrition_data';
+  final Map<String, Map<String, int>> _data = {};
 
   Future<void> init() async {
     final prefs = await SharedPreferences.getInstance();
     final jsonStr = prefs.getString(_key);
     if (jsonStr != null) {
-      final Map<String, dynamic> m = jsonDecode(jsonStr);
-      _data
-        ..clear()
-        ..addEntries(m.entries.map((e) => MapEntry(e.key, e.value as int)));
+      final Map<String, dynamic> decodedMap = jsonDecode(jsonStr);
+      _data.clear();
+      decodedMap.forEach((key, value) {
+        if (value is Map<String, dynamic>) {
+          _data[key] = value.cast<String, int>();
+        }
+      });
       _pruneOldData();
     }
   }
 
-  Future<void> addData(DateTime date, int value) async {
-    final key = date.toIso8601String().split('T')[0];
-    _data[key] = (_data[key] ?? 0) + value;
+  Future<void> addData(DateTime date, {int protein = 0, int fat = 0, int carbo = 0}) async {
+    final key = date.toIso8601String().split('T')[0]; 
+    final dailyData = _data.putIfAbsent(key, () => {'protein': 0, 'fat': 0, 'carbo': 0});
+
+    dailyData['protein'] = (dailyData['protein'] ?? 0) + protein;
+    dailyData['fat'] = (dailyData['fat'] ?? 0) + fat;
+    dailyData['carbo'] = (dailyData['carbo'] ?? 0) + carbo;
+    
     _pruneOldData();
     await _savePrefs();
+  }
+
+  Map<String, int>? getDataForDate(DateTime date) {
+    final key = date.toIso8601String().split('T')[0];
+    return _data[key];
   }
 
   void _pruneOldData() {
     final now = DateTime.now();
     _data.removeWhere((k, _) {
-      final d = DateTime.parse(k);
-      return now.difference(d).inDays >= 5;
+      try {
+        final d = DateTime.parse(k);
+        return now.difference(d).inDays >= 5;
+      } catch (e) {
+        return true; 
+      }
     });
   }
 
@@ -38,5 +54,5 @@ class DateValueStorage {
     await prefs.setString(_key, jsonEncode(_data));
   }
 
-  Map<String, int> get data => Map.unmodifiable(_data);
+  Map<String, Map<String, int>> get allData => Map.unmodifiable(_data);
 }
