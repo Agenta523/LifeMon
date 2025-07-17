881e92e36df80aeb6d3bc15d370c581818628d37
RyumaTaira <shin76447@gmail.com>
2025-07-17 10:09:26 +0900

    図鑑プレ

--- lib/presentation/screen/food_screen/dish_catalog_screen.dart
+++ lib/presentation/screen/food_screen/dish_catalog_screen.dart
@@ -26,7 +26,7 @@ class _DishCatalogScreenState extends State<DishCatalogScreen> {
   @override
   Widget build(BuildContext context) {
     final dishes = List.generate(
-      20, // ← デモ用に多めに
+      20,
       (index) => '${widget.dishCategory}${index + 1}',
     );
 
@@ -38,17 +38,19 @@ class _DishCatalogScreenState extends State<DishCatalogScreen> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(8),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // ← 横に2つ並べる
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
+                childAspectRatio: 3 / 2,
+              ),
               itemCount: dishes.length,
               itemBuilder: (context, index) {
                 final dish = dishes[index];
                 final isSelected = selectedDishes.contains(dish);
-                return ListTile(
-                  title: Text(dish),
-                  trailing:
-                      isSelected
-                          ? const Icon(Icons.check_circle, color: Colors.green)
-                          : const Icon(Icons.circle_outlined),
+                return GestureDetector(
                   onTap: () {
                     setState(() {
                       if (isSelected) {
@@ -58,6 +60,59 @@ class _DishCatalogScreenState extends State<DishCatalogScreen> {
                       }
                     });
                   },
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.orange[100],
+                          borderRadius: BorderRadius.circular(16),
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/images/${dish}.png',
+                            ), // 必要に応じて画像名調整
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 8,
+                        right: 8,
+                        child: Icon(
+                          isSelected
+                              ? Icons.check_circle
+                              : Icons.radio_button_unchecked,
+                          color: isSelected ? Colors.green : Colors.white,
+                          size: 28,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 4,
+                            horizontal: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.4),
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(16),
+                              bottomRight: Radius.circular(16),
+                            ),
+                          ),
+                          child: Text(
+                            dish,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             ),
