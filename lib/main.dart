77070a962db35639282df4035692751873953cb9
ikei-NITOC <ac254601@edu.okinawa-ct.ac.jp>
2025-07-24 10:06:36 +0900

    バックエンドデータでホームスクリーンを更新

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'presentation/navigator/analyze_navigator.dart';
 import 'presentation/navigator/home_navigator.dart';
 import 'presentation/navigator/food_navigator.dart';
 import 'presentation/widget/bottom_navigation.dart';
-
-// 1. HomeScreenのStateを参照するためにインポートを追加
 import 'presentation/screen/home_screen/home_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -34,26 +32,23 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 1;
 
-  // 2. HomeScreenのStateにアクセスするためのGlobalKeyを作成
   final GlobalKey<HomeScreenState> _homeScreenKey =
       GlobalKey<HomeScreenState>();
 
-  // 3. _screensリストをfinalではなくし、non-constにする
   late final List<Widget> _screens;
 
   @override
   void initState() {
     super.initState();
-    // 4. initStateで_screensを初期化し、HomeNavigatorにkeyを渡す
+
     _screens = [
       const AnalyzeNavigator(),
-      HomeNavigator(homeScreenKey: _homeScreenKey), // ◀️ ここでKeyを渡す
+      HomeNavigator(homeScreenKey: _homeScreenKey),
       const FoodNavigator(),
     ];
   }
 
   void _onItemTapped(int index) {
-    // 5. ホームタブ（インデックス1）がタップされ、かつそれが現在のタブでない場合に更新
     if (index == 1 && _currentIndex != 1) {
       _homeScreenKey.currentState?.loadTodaysNutrition();
     }
@@ -68,7 +63,6 @@ class _MainScreenState extends State<MainScreen> {
       body: IndexedStack(index: _currentIndex, children: _screens),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: _currentIndex,
-        // 6. 更新したメソッドを渡す
         onItemTapped: _onItemTapped,
       ),
     );
