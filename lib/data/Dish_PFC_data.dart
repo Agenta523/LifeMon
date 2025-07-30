641f6b67f253fe39eabadffad7445981d338fd48
RyumaTaira <shin76447@gmail.com>
2025-07-30 09:54:12 +0900

    栄養データ

--- lib/data/Dish_PFC_data.dart
+++ lib/data/Dish_PFC_data.dart
@@ -1,10 +1,11 @@
 const Map<String, Map<String, int>> dishPFCData = {
-  "ネギ塩炒め": {"protein": 15, "fat": 10, "carbo": 5},
-  "唐揚げ": {"protein": 20, "fat": 18, "carbo": 10},
-  "ハンバーグ": {"protein": 22, "fat": 15, "carbo": 12},
-  "サラダ": {"protein": 1, "fat": 2, "carbo": 3},
-  "きんぴらごぼう": {"protein": 2, "fat": 3, "carbo": 10},
-  "ポテトサラダ": {"protein": 3, "fat": 6, "carbo": 15},
-  "味噌汁": {"protein": 2, "fat": 1, "carbo": 3},
-  "コーンスープ": {"protein": 3, "fat": 4, "carbo": 10},
+  "白米": {"protein": 3, "fat": 0, "carbo": 55}, // 白米150g相当
+  "ネギ塩炒め": {"protein": 15, "fat": 12, "carbo": 8}, // 豚肉炒め＋タレ
+  "唐揚げ": {"protein": 17, "fat": 14, "carbo": 10}, // 鶏もも3個程度
+  "ハンバーグ": {"protein": 18, "fat": 15, "carbo": 12}, // 100gの合挽き＋ソース
+  "サラダ": {"protein": 1, "fat": 3, "carbo": 4}, // 野菜＋ドレッシング
+  "きんぴらごぼう": {"protein": 2, "fat": 4, "carbo": 13}, // ごぼう＋にんじん＋油
+  "ポテトサラダ": {"protein": 3, "fat": 9, "carbo": 18}, // じゃがいも＋マヨネーズ
+  "味噌汁": {"protein": 2, "fat": 1, "carbo": 3}, // 豆腐やわかめ入り
+  "コーンスープ": {"protein": 3, "fat": 5, "carbo": 15}, // 150ml程度
 };
